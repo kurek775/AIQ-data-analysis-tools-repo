6faf7bbe4671a62ef59fce3661cf32192c314494
kurek775 <pavelkurek@seznam.cz>
2024-06-11 06:19:24 +0200

    WIP - detective

---
+++ check_conf.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def main():
+
+    gen = pd.read_csv('parameter_space_analysis.csv')
+    df = pd.read_csv('basic_config_results_.csv')
+    if 'Config' not in gen.columns:
+        gen['Config'] = gen['q'].astype(str) + '_' + gen['lambda'].astype(str) + '_' + gen['alpha'].astype(str) + '_' + gen['epsilon'].astype(str) + '_' + gen['gamma'].astype(str)
+    print(df)
+    print(gen)
+
+    for config in gen['Config']:
+        if config not in df['Config'].values:
+            print(f"Missing config: {config}")    
+
+
+if __name__ == "__main__":
+    main()
--- parameter_space_generator.py
+++ parameter_space_generator.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def main():
     no_l_config = set()
+    l_config = set()
     with open('parameter_space_analysis.log', 'w') as f:
                 l_values = np.linspace(0.4, 0.9, 3)
                 a_values = np.linspace(0.1, 0.55, 3)
@@ -13,12 +14,14 @@ def main():
                     for a_ in a_values:
                         for e_ in e_values:
                             for g_ in g_values:
-                                f.write(f"Sarsa_l,0,{l_:.2f},{a_:.2f},{e_:.3f},{g_:.2f}\n")
+                                l_config.add(f"Sarsa_l,0,{l_:.2f},{a_:.2f},{e_:.3f},{g_:.2f}")
                                 no_l_config.add(f"Sarsa_l,0,0,{a_:.2f},{e_:.3f},{g_:.2f}")
             
     
                 for config in no_l_config:
                     f.write(f"{config}\n")
 
+                for config in l_config:
+                    f.write(f"{config}\n")
 if __name__ == "__main__":
     main()
