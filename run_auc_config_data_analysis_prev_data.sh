2f2446779b71ffb6fc27e2b355e436415e3c9d89
kurek775 <pavelkurek@seznam.cz>
2024-06-27 09:56:26 +0200

    DONE - bash scripts

--- run_auc_config_data_analysis_prev_data.sh
+++ run_auc_config_data_analysis_prev_data.sh
@@ -1,3 +1,4 @@
-python previous_agents_analysis.py --file='experiment_result.csv'
+python previous_agents_formater.py --file='experiment_result.csv'
 python configurations_analysis.py --file='vysledky_el.csv,formated_prev_data.csv'
-python auc_analysis.py --file='vysledky_el.csv,formated_prev_data.csv'
+#python auc_same_configs_analysis.py --file='vysledky_el.csv,formated_prev_data.csv'
+python auc_best_configs_analysis.py --file='vysledky_el.csv'
--- run_basic_data_analysis.sh
+++ run_basic_data_analysis.sh
@@ -1,2 +1,2 @@
 python configurations_analysis.py --file='vysledky_el_agent.csv'
-python agent_basic_analysis.py --file='basic_config_results.csv'
+python agent_desc_statistics.py --file='basic_config_results.csv'
